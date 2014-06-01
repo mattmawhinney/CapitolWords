163bc58802c467c956f48e5a959fe058b6ef4972
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-06-01 17:03:19 -0400

    modified views

---
+++ scripts/ajax_search.js
@@ -0,0 +1,22 @@
+function getXmlHttpRequestObject() {
+	if (window.XMLHttpRequest) {
+		return new XMLHttpRequest();
+	} else if(window.ActiveXObject) {
+		return new ActiveXObject("Microsoft.XMLHTTP");
+	} else {
+		alert("Your Browser Sucks!\nIt's about time to upgrade don't you think?");
+	}
+}
+
+
+var searchReq = getXmlHttpRequestObject();
+
+
+function searchSuggest() {
+	if (searchReq.readyState == 4 || searchReq.readyState == 0) {
+		var str = escape(document.getElementById('txtSearch').value);
+		searchReq.open("GET", 'suggest.rb?search=' + str, true);
+		searchReq.onreadystatechange = handleSearchSuggest; 
+		searchReq.send(null);
+	}		
+}
